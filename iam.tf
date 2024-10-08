d468297719629d52fa127742da3aaa6dc89197a1
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-09 00:30:10 +0100

    add eks svc cluster role

--- iam.tf
+++ iam.tf
@@ -2,3 +2,25 @@
 ### EKS Cluster Role ###
 ########################
 
+resource "aws_iam_role" "eks_svc_role" {
+  name               = "${var.project}-cluster-role"
+  assume_role_policy = "TBC"
+  tags               = local.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      tags["AutoTag_CreateTime"],
+      tags["AutoTag_Creator"],
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_svc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
+  role       = aws_iam_role.eks_svc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
