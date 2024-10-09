599117c1c7d8639ebc994dc97d68e3dffa0872d0
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-09 01:21:46 +0100

    add subnets to eks cluster vpc config

--- eks.tf
+++ eks.tf
@@ -9,7 +9,7 @@ resource "aws_eks_cluster" "control_plane" {
   role_arn = aws_iam_role.eks_svc_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+    subnet_ids = local.all_subnets
   }
 
   
