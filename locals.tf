a73e08e39cdc39736bc5bf2f4cd6ff74471cfc78
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-09 01:21:00 +0100

    add subnet summary to local variables

--- locals.tf
+++ locals.tf
@@ -7,3 +7,9 @@ locals {
 
   retention_in_days = var.environment == "development" ? 1 : 30
 }
+
+locals {
+  ec2_subnets    = data.terraform_remote_state.vpc_imports.outputs.ec2_nodes_private_subnets
+  fargate_subnets = data.terraform_remote_state.vpc_imports.outputs.fargate_nodes_private_subnets
+  all_subnets     = concat(local.ec2_subnets, local.fargate_subnets)
+}
