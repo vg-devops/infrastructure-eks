f07878afb8efc3d5b4c7b39a5963cc72411479cf
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-10 00:09:53 +0100

    add policy and role for aws lb controller, add assume document for Federated principal/alb service account

---
+++ iam_alb.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role_policy.json
+  name               = "aws-load-balancer-controller"
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  policy = file("./Policy_AWSLoadBalancerController.json") ## from version 2.7.2 as stated on AWS document website
+  name   = "AWSLoadBalancerController"
+  description = "ref: https://docs.aws.amazon.com/eks/latest/userguide/lbc-helm.html"
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
