f952b59b54244cc9b5ab3791724cfdf7fd513bc8
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-09 02:30:51 +0100

    fix state import for subnet ids from vpc infra

--- fargate.tf
+++ fargate.tf
@@ -2,7 +2,7 @@ resource "aws_eks_fargate_profile" "eks_fargate_general" {
   cluster_name           = aws_eks_cluster.control_plane.name
   fargate_profile_name   = "${var.project}-fargate-profile"
   pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
-  subnet_ids             = data.terraform_remote_state.vpc.outputs.fargate_nodes_private_subnets
+  subnet_ids             = data.terraform_remote_state.vpc_imports.outputs.fargate_nodes_private_subnets
 
   selector {
     namespace = "fargate-ns"
