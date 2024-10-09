7b21d1c31c3a933fbe95ca3518424e843ebe20ac
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-09 02:29:15 +0100

    add basic fargate profile and the role for pods exec

---
+++ fargate.tf
@@ -0,0 +1,21 @@
+resource "aws_eks_fargate_profile" "eks_fargate_general" {
+  cluster_name           = aws_eks_cluster.control_plane.name
+  fargate_profile_name   = "${var.project}-fargate-profile"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
+  subnet_ids             = data.terraform_remote_state.vpc.outputs.fargate_nodes_private_subnets
+
+  selector {
+    namespace = "fargate-ns"
+    labels = {
+      "fargate" = "true"
+    }
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      "Name" = "${var.project}-fargate-profile"
+    }
+  )
+}
+
--- iam.tf
+++ iam.tf
@@ -41,3 +41,30 @@ data "aws_iam_policy_document" "eks_assume_role_policy" {
     effect  = "Allow"
   }
 }
+
+
+###################
+##### FARGATE #####
+###################
+
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name = "eks-fargate-pod-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks-fargate-pods.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_pod_execution_role.name
+}
