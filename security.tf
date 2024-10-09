45fbde347f0f3bb2b14ef74504c69aacd751fc3f
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-09 01:59:36 +0100

    add cluster security group for control plane communication

---
+++ security.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "eks_cluster" {
+  name        = "${var.project}-cluster-sg"
+  description = "Security group for EKS cluster"
+  vpc_id      = data.terraform_remote_state.vpc_imports.outputs.vpc_id
+
+  # inbound traffic from any VPC nodes
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = data.terraform_remote_state.vpc_imports.outputs.vpc_cidr
+  }
+
+  # all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      "Name" = "${var.project}-cluster-sg"
+    }
+  )
+}
