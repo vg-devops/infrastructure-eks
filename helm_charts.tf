6ac5c0cd7c24b78fca16e170cb67ec8c08791682
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-10 01:54:30 +0100

    add helm chart for load balancer

--- helm_charts.tf
+++ helm_charts.tf
@@ -9,5 +9,37 @@ resource "kubernetes_service_account" "aws_load_balancer_controller" {
   depends_on = [data.aws_eks_cluster.cluster]
 }
 
+resource "helm_release" "aws-load-balancer-controller" {
+  name = "aws-load-balancer-controller"
 
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.9.0"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.control_plane.id
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.9.0"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.aws_load_balancer_controller.arn
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.aws_load_balancer_controller_attach,
+    kubernetes_service_account.aws_load_balancer_controller
+  ]
+}
 
