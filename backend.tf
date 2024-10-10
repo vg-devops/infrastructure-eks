c04e7104391bc28ee3954840da0a8e0e32faa437
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-10 01:56:03 +0100

    fix trial: use token instead of exec for helm provider

--- backend.tf
+++ backend.tf
@@ -13,11 +13,7 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.control_plane.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.control_plane.certificate_authority[0].data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.control_plane.id]
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }  
 }
 
