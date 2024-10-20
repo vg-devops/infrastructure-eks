d7c0bf244932b151759fb7d0a6560dafba939879
Vagif Gafarov <v_k_special@yahoo.com>
2024-10-20 23:20:27 +0100

    add aws_caller_identity to obtain current account data

--- data_resources.tf
+++ data_resources.tf
@@ -5,3 +5,7 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = "${var.project}-cluster"
 }
+
+data "aws_caller_identity" "current_account" {
+  # used to obtain current account id
+}
